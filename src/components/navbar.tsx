26ba06a8d2d3739a2aebcd48e078031192ea06a8
b3ni15 <ballobeni08@gmail.com>
2025-03-26 23:43:51 +0100

    Update Navbar component styles and branding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,10 +3,10 @@ import { ThemeSwitch } from "@/components/theme-switch";
 export const Navbar = () => {
   return (
     <>
-      <nav className="flex items-center justify-between py-4 px-6 bg-white shadow-md">
+      <nav className="flex items-center justify-between py-4 px-6 bg-[#0F1015]/80 backdrop-blur-md shadow-md">
         <div className="flex items-center gap-4">
-          <a href="/" className="text-lg font-bold text-gray-800">
-            Heroui
+          <a href="/" className="text-lg font-bold text-white">
+            devbeni.lol
           </a>
         </div>
         <ThemeSwitch />
