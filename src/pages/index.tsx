00d476f006c6e4f84c856fc41c13b17aefa56454
b3ni15 <ballobeni08@gmail.com>
2025-03-28 12:08:06 +0100

    Remove unused SVG and type files; refactor IndexPage layout and add social media icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,29 @@ import { button as buttonStyles } from "@heroui/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+// Import github and instagramm with reacticons
+import { FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 max-w-4xl">
-          <img src="/Ellipse.svg" alt="Decorative graphic" className="w-96 h-96" />
-          
+          <div className="relative w-96 h-96">
+            <img
+              src="/Ellipse.svg"
+              alt="Decorative background"
+              className="absolute w-full h-full"
+            />
+            <div className="relative w-52 h-52 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <img
+                src="/pfp.png"
+                alt="Profile picture"
+                className="absolute w-full h-full z-10 object-cover"
+              />
+              <img src="/hi.png" alt="" className="absolute -bottom-7 -right-7 transform -translate-x-1/2 -translate-y-1/2 w-14 h-14 z-10 object-cover" />
+            </div>
+          </div>
           <div className="text-center md:text-left">
             <div className="inline-block max-w-lg">
               <span className={title()}>Make&nbsp;</span>
@@ -29,14 +44,8 @@ export default function IndexPage() {
             <div className="flex gap-3 justify-center md:justify-start mt-6">
               <Link
                 isExternal
-                className={buttonStyles({
-                  color: "primary",
-                  radius: "full",
-                  variant: "shadow",
-                })}
-                href={siteConfig.links.docs}
-              >
-                Documentation
+                className={buttonStyles({color: "primary", radius: "full", variant: "shadow" })} href={siteConfig.links.docs}>
+                <FaGithub className="h-5 w-5" /> Documentation
               </Link>
               <Link
                 isExternal
